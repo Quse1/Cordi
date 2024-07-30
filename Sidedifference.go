5aed3901ee8431ebf7bf9d9ee6172893ed35716a
Quse1 <39953087+Quse1@users.noreply.github.com>
2024-07-30 11:16:46 +0300

    Update Sidedifference.go

--- Sidedifference.go
+++ Sidedifference.go
@@ -3,32 +3,31 @@ package main
 import "fmt"
 
 func main() {
-	var n int
-	_, _ = fmt.Scan(&n)
-	// Создание массива
-	arr := make([][]int, n)
-	for i := range arr {
-		arr[i] = make([]int, n)
-	}
-
-	// Заполнение массива
-	for i := 0; i < n; i++ {
-		for j := 0; j < n; j++ {
-
-			if i+j == n-1 {
-				arr[i][j] = 1 // Элемент на побочной диагонали
-			} else if i+j < n-1 {
-				arr[i][j] = 0 // Элемент выше побочной диагонали
-			} else {
-				arr[i][j] = 2 // Элемент ниже побочной диагонали
-			}
-		}
-	}
-	// Вывод массива
-	for i := 0; i < n; i++ {
-		for j := 0; j < n; j++ {
-			fmt.Printf("%d ", arr[i][j])
-		}
-		fmt.Println()
-	}
+    var n int
+    _, _ = fmt.Scan(&n)
+    // Создание массива
+    a := make([][]int, n)
+    for i := 0; i < len(a); i++ {
+        a[i] = make([]int, n)
+    }
+    
+    // Заполнение массива
+    for i:=0; i < n; i++  {
+        for j:=0; j < n; j++ {
+            if i + j == n - 1 {
+                a[i][j] = 1 // Элемент на побочной диагонали
+            } else if i + j < n - 1 {
+                a[i][j] = 0 // Элемент выше побочной диагонали
+            } else {
+                a[i][j] = 2 // Элемент ниже побочной диагонали
+            }
+    }
+}
+// Вывод массива
+ for i:=0; i < n; i++  {
+        for j:=0; j < n; j++ {
+            fmt.Print(a[i][j], " ")
+        }
+        fmt.Println()
+    }
 }
