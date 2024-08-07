d9dea2cf97fa0b591fa4b59dccd5b9395bd640b2
Quse1 <39953087+Quse1@users.noreply.github.com>
2024-08-07 11:31:10 +0300

    Update testStepik.go

--- testStepik.go
+++ testStepik.go
@@ -9,7 +9,7 @@ func main() {
     var n int 
     var n2 int
     var n3 int
-    //var count int
+    var count int
     //var sum int 
     _,_ = fmt.Scan(&n)
     s:= make([]string, n)
@@ -20,19 +20,34 @@ func main() {
         snum = append(snum, num)
     }
     
-    for j:= 0; j < len(snum); j++ {
+    for j:= 0; j < len(snum)-1; j++ {
+        
         for k:= j+1; k < len(snum); k++ {
-            //fmt.Println(snum[j], snum[k])
-            if snum[j] < 3 && snum[k] < 3 && snum[j] != 1 && snum[k] != 1   {
+            fmt.Println(snum[j], snum[k])
+            if snum[j] == 2 || snum[k] == 2 {
+                count++
                 if snum[j] == snum[k] {
                     n2 = snum[j] + snum[k]
-                    //fmt.Println(snum[j], snum[k])
-                    fmt.Println(n2)
+                    if n2 == 4 {
+                        count--
+                        //fmt.Println(count)
+                    }
             }
         } else if snum[j] == 3 && snum[k] == 1 || snum[j] == 1 && snum[k] == 3 {
                 n3 = snum[j] + snum[k]
-                fmt.Println(n3)
-            }
+                if n3 == 4 {
+                    count++
+                    //fmt.Println(count)
+                }
+        } else if snum[j] == 3 || snum[k] == 3 {
+                    count++
+                    //fmt.Println(count)
+        } else if snum[j] == 4 || snum[k] == 4 {
+                count++
+                //fmt.Println(count)
+        }
+    }
+    
 }
-}
+fmt.Println(count)
 }
