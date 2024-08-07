d1c8f52b68553db595d4b598167c41df481ca43d
Quse1 <39953087+Quse1@users.noreply.github.com>
2024-08-07 14:30:01 +0300

    Update testStepik.go

--- testStepik.go
+++ testStepik.go
@@ -8,24 +8,30 @@ import (
 func main() {
     var n int 
     var n2 int
-    var n3 int
+    var n31 int
+    var n21 int
     var count int
     //var sum int 
     _,_ = fmt.Scan(&n)
     s:= make([]string, n)
-    var snum []int
+    snum := make([]int, n)
+    
     for i:=0; i < len(s); i++ {
         _, _ = fmt.Scan(&s[i])
         num, _ := strconv.Atoi(s[i])
-        snum = append(snum, num)
+        snum[i] = num
     }
     
-    for j:= 0; j < len(snum)-1; j++ {
-        
+    if n == 1 {
+        count++ 
+    } else {
+      for j:= 0; j < len(snum)-1; j++ {
         for k:= j+1; k < len(snum); k++ {
-            fmt.Println(snum[j], snum[k])
-            if snum[j] == 2 || snum[k] == 2 {
+            //fmt.Println(snum[j], snum[k])
+            if snum[j] == 2 && snum[k] == 2 {
                 count++
+                fmt.Println(count)
+                //fmt.Println(count)
                 if snum[j] == snum[k] {
                     n2 = snum[j] + snum[k]
                     if n2 == 4 {
@@ -34,20 +40,27 @@ func main() {
                     }
             }
         } else if snum[j] == 3 && snum[k] == 1 || snum[j] == 1 && snum[k] == 3 {
-                n3 = snum[j] + snum[k]
-                if n3 == 4 {
+                n31 = snum[j] + snum[k]
+                if n31 == 4 {
                     count++
                     //fmt.Println(count)
                 }
-        } else if snum[j] == 3 || snum[k] == 3 {
+        } else if snum[j] == 3 && snum[k] == 3 {
                     count++
                     //fmt.Println(count)
-        } else if snum[j] == 4 || snum[k] == 4 {
+        } else if snum[j] == 4 && snum[k] == 4 {
                 count++
                 //fmt.Println(count)
+        } else if snum[j] == 2 && snum[k] == 1 || snum[j] == 1 && snum[k] == 2 {
+            n21 = snum[j] + snum[k]
+            if n21 == 3 {
+                count++
+            }
         }
     }
+}  
+    }
     
-}
 fmt.Println(count)
+
 }
