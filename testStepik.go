2b98a95dfbb13c61a94f91322ad77f342c0408f5
Quse1 <39953087+Quse1@users.noreply.github.com>
2024-08-09 16:51:50 +0300

    Update testStepik.go

--- testStepik.go
+++ testStepik.go
@@ -8,7 +8,6 @@ import (
 func main() {
     var n int
     var count int
-    var count1 int
     _,_ = fmt.Scan(&n)
     s:= make([]string, n)
     snum := make(map[int]int, n)
@@ -30,51 +29,56 @@ func main() {
     if n == 1 {
         count = 1
     } else {
+        //fmt.Println(snum)
         for snum[4] > 0 {
         count++
         snum[4]--
     }
-    if snum[3] > 0 && snum[1] > 0 {
-        for snum[3] + snum[1] > 0 {
+    if snum[3] > 0  {
+        for snum[3] > 0 {
         count++
         snum[3]--
-        snum[1]--
-        
+        if snum[1] > 0 {
+            snum[1]--
+        }
     }
-    
-    } else if snum[3] > 0 && snum[1] <= 0 {
-        count+=snum[3]
-        snum[3]--
     }
     if snum[2] >= 2 {
         for snum[2] > 1 {
             count+= snum[2] / 2 
             snum[2]-=2
            }
-    } else if snum[2] > 0 && snum[1] > 0 {
-        for snum[2] + snum[1] > 0 {
+    }
+    if snum[2] > 0  {
+        for snum[2] > 0  {
         count++
         snum[2]--
-        snum[1]--
+        if snum[1] > 0  {
+            for snum[1] > 0 {
+                snum[1]--
+            }
+            
         }
-        
-     } else if snum[3] <= 0 && snum[2] <= 0 && snum[1] >= 4 {
+        }
+     }
+     if snum[1] >= 4 {
         count += snum[1] / 4
-        
-     } 
-     if snum[2] == 1 && snum[3] <= 0 && snum[4] <= 0 && snum[1] <= 0 {
-         count+=snum[2]
-         snum[2]-- 
      }
      
-     if snum[1] <= 3 && snum[3] <= 0 && snum[4] <= 0 && snum[2] <= 0 {
-          //fmt.Println(snum[1])
-            count1 = snum[1]
-            snum[1] =0
-         if count1 == 3 || count1 == 2 || count1 ==1 {
-             count++
-         }
+     if snum[1] <= 2 && snum[1] != 0   {
+        count++
+        snum[1] = 0
+        if snum[2] > 0 {
+            snum[2]--
+        }
      }
+     
+     if snum[1] == 3 {
+         count++
+         snum[1] = 0
+     }
+     
     }
+    //fmt.Println(snum)
     fmt.Println(count)
 }
