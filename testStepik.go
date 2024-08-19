25725f6d1633dc1ce54aca9c039bf5e16d08944b
Quse1 <39953087+Quse1@users.noreply.github.com>
2024-08-19 17:52:44 +0300

    Update testStepik.go

--- testStepik.go
+++ testStepik.go
@@ -1,35 +1,29 @@
-// Online Go compiler to run Golang program online
-// Print "Try programiz.pro" message
-
 package main
+
 import (
     "fmt"
-    "bufio"
-    "os"
-    "strings"
-    
-    )
+    _"strings"
+)
 
-func alphabet (t []string) string {
-    var res string
-    var alf []string = []string{"_", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
+func main() {
+    //var n int
+    var s string
+   // _, _ = fmt.Scan(&n)
+    _, _ = fmt.Scan(&s)
+    var count int
     
-    for i:=0; i < len(alf); i++ {
-        for j:=i; j < i+1; j++ {
-            if alf[i] == t[j] {
-                res = t[j]
+    for i:=0; i < len(s); i+=2 {
+        end := i + 2
+        if len(s) < end {
+            end = len(s)
+        }
+        //fmt.Println(s[i:end])
+        if s[i] != 'x' {
+            if s[i:end] == "xx" {
+            fmt.Println(s[i:end])
+            count = 0
             }
         }
     }
-    return res
-}
-
-func main() {
-    input := bufio.NewScanner(os.Stdin)
-    _ = input.Scan()
-    text := input.Text()
-    t := strings.Split(text, "")
-    fmt.Println(alphabet(t))
-    
-    
+    fmt.Println(count)
 }
