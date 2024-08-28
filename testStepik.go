67cf104377c1b30b1a8a5bbfdec5f9d514143bbd
Quse1 <39953087+Quse1@users.noreply.github.com>
2024-08-28 17:44:40 +0300

    Update testStepik.go

--- testStepik.go
+++ testStepik.go
@@ -1,29 +1,56 @@
 package main
 
 import (
-    "fmt"
-    _"strings"
+	"bufio"
+	"fmt"
+	"os"
 )
 
 func main() {
-    //var n int
-    var s string
-   // _, _ = fmt.Scan(&n)
-    _, _ = fmt.Scan(&s)
-    var count int
-    
-    for i:=0; i < len(s); i+=2 {
-        end := i + 2
-        if len(s) < end {
-            end = len(s)
-        }
-        //fmt.Println(s[i:end])
-        if s[i] != 'x' {
-            if s[i:end] == "xx" {
-            fmt.Println(s[i:end])
-            count = 0
-            }
-        }
-    }
-    fmt.Println(count)
+	symb := map[string]int {
+	    "(": 0,
+	    ")": 0,
+	    "[": 0,
+	    "]": 0,
+	    "{": 0,
+	    "}": 0,
+	}
+	var count int
+	input := bufio.NewScanner(os.Stdin)
+	_ = input.Scan()
+	text := input.Text()
+	//fmt.Println(text)
+	
+	for i:=0; i < len(text); i++ {
+	    switch string(text[i]) {
+	        case "(":
+	        symb["("]++
+	        case ")":
+	        symb[")"]++
+	        case "[":
+	        symb["["]++
+	        case "]":
+	        symb["]"]++
+	        case "{":
+	        symb["{"]++
+	        case "}":
+	        symb["}"]++
+	    }
+	}
+	
+	
+	if symb["("] == symb[")"] {
+	    count++
+	}
+	if symb["["] == symb["]"] {
+	    count++
+	}
+	if symb["{"] == symb["}"] {
+	    count++
+	}
+	
+	if count == 3 {
+	    fmt.Println("Success")
+	}
+
 }
